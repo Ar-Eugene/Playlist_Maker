e222b31024006a73cb386e2d1e7407bd54ee80c3
dev_Ar <artemenkoe2711@gmail.com>
2024-10-10 19:26:24 +0400

    Заложили фундамент для работы с DI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("androidx.core:core-ktx:1.9.0")
+    implementation("io.insert-koin:koin-android:4.0.0")
 }
